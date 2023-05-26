abad498d2091b189e9eb13d1f68c68eb479bbf0f
MiguelArbelaez0 <119169682+MiguelArbelaez0@users.noreply.github.com>
2023-05-25 22:21:46 -0500

    --

--- products_store_app/lib/data/remote_data_source/remote_data_source.dart
+++ products_store_app/lib/data/remote_data_source/remote_data_source.dart
@@ -1,11 +1,6 @@
-import 'package:products_store_app/data/repository_impl/impl_repositori.dart';
-import 'package:products_store_app/domain/repository/products_repository.dart';
 import 'package:http/http.dart' as http;
 
-class RemoteDataSource extends ProductsRepository {
-  late final ProductsRepositoryImpl productsRepositoryImpl;
-
-  @override
+class RemoteDataSource {
   Future<void> onGetProduct() async {
     Uri url = Uri.parse('https://fakestoreapi.com/products');
 
--- products_store_app/lib/data/repository_impl/impl_repositori.dart
+++ products_store_app/lib/data/repository_impl/impl_repositori.dart
@@ -1,8 +1,14 @@
+import 'package:products_store_app/data/remote_data_source/remote_data_source.dart';
 import 'package:products_store_app/domain/repository/products_repository.dart';
 
 class ProductsRepositoryImpl extends ProductsRepository {
+  final RemoteDataSource _remoteDataSource;
+
+  ProductsRepositoryImpl({this._remoteDataSource});
+
   @override
   void onGetProduct() {
     // TODO: implement onGetProduct
+    _remoteDataSource.onGetProduct();
   }
 }
