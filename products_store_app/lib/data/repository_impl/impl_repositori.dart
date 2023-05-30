3a3e92c2324573e8b3edfb0e6ef98322f0a681c6
MiguelArbelaez0 <119169682+MiguelArbelaez0@users.noreply.github.com>
2023-05-29 23:26:43 -0500

    --

--- products_store_app/lib/data/repository_impl/impl_repositori.dart
+++ products_store_app/lib/data/repository_impl/impl_repositori.dart
@@ -2,12 +2,12 @@ import 'package:products_store_app/data/remote_data_source/remote_data_source.da
 import 'package:products_store_app/domain/repository/products_repository.dart';
 
 class ProductsRepositoryImpl extends ProductsRepository {
-  final RemoteDataSource remoteDataSource;
+  final RemoteDataSource _remoteDataSource;
 
-  ProductsRepositoryImpl({required this.remoteDataSource});
+  ProductsRepositoryImpl(this._remoteDataSource);
 
   @override
   void onGetProduct() {
-    remoteDataSource.onGetProduct();
+    _remoteDataSource.onGetProduct();
   }
 }
