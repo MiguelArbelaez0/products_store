e98bf6a6dd9650bd62747a22e87db2f9de87d0b9
MiguelArbelaez0 <119169682+MiguelArbelaez0@users.noreply.github.com>
2023-08-21 18:25:05 -0500

    funciona

--- products_store_app/test/add_product_test.dart
+++ products_store_app/test/add_product_test.dart
@@ -3,22 +3,44 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:products_store_app/presentation/screens/home_screen.dart';
 import 'package:products_store_app/presentation/screens/widgets/product_widget.dart';
 
+// void main() {
+//   testWidgets('Test de botón "add" en ProductWidget',
+//       (WidgetTester tester) async {
+//     // final CartViewModel cartViewModel = CartViewModel();
+//     await tester.pumpWidget(
+//       MaterialApp(home: HomeScreen()),
+//     );
+
+//     await tester.pumpAndSettle();
+
+//     final addButton = find.byIcon(Icons.add);
+
+//     expect(addButton, findsOneWidget);
+
+//     await tester.tap(addButton);
+
+//     await tester.pumpAndSettle();
+//   });
+// }
 void main() {
-  testWidgets('Test de botón "add" en ProductWidget',
-      (WidgetTester tester) async {
-    // final CartViewModel cartViewModel = CartViewModel();
-    await tester.pumpWidget(
-      MaterialApp(home: HomeScreen()),
-    );
+  testWidgets(
+    'Test button tap in ProductWidget',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: HomeScreen(),
+      ));
 
-    await tester.pumpAndSettle();
+      await tester.pump();
 
-    final addButton = find.byIcon(Icons.add);
+      final productWidgetFinder = find.byType(ProductWidget);
+      final productWidgetInstances = tester.widgetList(productWidgetFinder);
 
-    expect(addButton, findsOneWidget);
+      for (var productWidgetInstance in productWidgetInstances) {
+        await tester.tap(find.byWidget(productWidgetInstance));
+        await tester.pump();
 
-    await tester.tap(addButton);
-
-    await tester.pumpAndSettle();
-  });
+        expect(find.text('Button tapped'), findsOneWidget);
+      }
+    },
+  );
 }
