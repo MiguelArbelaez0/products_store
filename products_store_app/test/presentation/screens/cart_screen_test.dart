12cda47ce31f223cff912c48fe07e622db4dcce4
MiguelArbelaez0 <119169682+MiguelArbelaez0@users.noreply.github.com>
2023-08-23 17:31:57 -0500

    --

---
+++ products_store_app/test/presentation/screens/cart_screen_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Verifica que el widget de total muestre el valor correcto',
+      (WidgetTester tester) async {});
+}
