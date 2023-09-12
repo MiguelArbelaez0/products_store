aa0e8bb908ee98d16c92b9cbf5e22068be71ac85
MiguelArbelaez0 <119169682+MiguelArbelaez0@users.noreply.github.com>
2023-09-12 17:25:26 -0500

    -_-

--- products_store_app/integration_test/app_test.dart
+++ products_store_app/integration_test/app_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:products_store_app/main.dart';
@@ -6,8 +7,22 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group("end-to-end test", () {
-    testWidgets("", (tester) async {
+    testWidgets("add-product", (tester) async {
       await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
+      final addProduct = find.byKey(const Key("product-widget"));
+      await tester.pumpAndSettle();
+      expect(addProduct, findsOneWidget);
+    });
+    testWidgets("increment-product", (tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
+
+      final incrementProduct = find.byKey(
+        const Key("increment-product"),
+      );
+      await tester.pumpAndSettle();
+      expect(incrementProduct, findsOneWidget);
     });
   });
 }
